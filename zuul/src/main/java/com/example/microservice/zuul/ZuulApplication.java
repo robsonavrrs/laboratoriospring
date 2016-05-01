3788df32bb2be74a6e3afca10cfbe6975fcca02f
robson <robson@monstro>
2016-04-30 21:00:11 -0300

    Integração com o eureka.

--- zuul/src/main/java/com/example/microservice/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/example/microservice/zuul/ZuulApplication.java
@@ -1,9 +1,15 @@
-package com.example.microservice;
+package com.example.microservice.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
+@Controller
+@EnableZuulProxy
+@EnableDiscoveryClient
 public class ZuulApplication {
 
 	public static void main(String[] args) {
--- zuul/src/test/java/com/example/microservice/ZuulApplicationTests.java
+++ zuul/src/test/java/com/example/microservice/ZuulApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.microservice;
 
+import com.example.microservice.zuul.ZuulApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
