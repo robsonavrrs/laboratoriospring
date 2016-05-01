4140d0f826d518de02ab71c3ea4548d0e0b88c30
robson <robson@monstro>
2016-05-01 09:50:20 -0300

    Ajuste de formatação.

--- zuul/src/main/java/com/example/microservice/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/example/microservice/zuul/ZuulApplication.java
@@ -1,6 +1,5 @@
 package com.example.microservice.zuul;
 
-import com.netflix.loadbalancer.LoadBalancerContext;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -13,9 +12,7 @@ import org.springframework.stereotype.Controller;
 @EnableDiscoveryClient
 public class ZuulApplication {
 
-    private LoadBalancerContext teste;
-    
-	public static void main(String[] args) {
-		new SpringApplicationBuilder(ZuulApplication.class).web(true).run(args);
-	}
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ZuulApplication.class).web(true).run(args);
+    }
 }
